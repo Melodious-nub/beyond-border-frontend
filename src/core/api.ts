df7fb34eed2fde9a382a33d5bf65f5b8df495170
Shawon Talukder <shawon.talukder@green-delta.com>
2025-10-05 17:50:36 +0600

    updated with api.ts

--- src/core/api.ts
+++ src/core/api.ts
@@ -1,8 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Api {
+  private readonly baseUrl = 'localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  // auth endpoints
+  login(data: any) {
+    return this.http.post(`${this.baseUrl}/login`, data);
+  }
+
+  me() {
+    return this.http.get(`${this.baseUrl}/me`);
+  }
+
+  updateProfile(data: any) {
+    return this.http.put(`${this.baseUrl}/profile`, data);
+  }
   
 }
