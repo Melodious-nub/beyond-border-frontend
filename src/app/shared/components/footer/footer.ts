02a24dbe475b11ac0bd007ed81bb7bad657f24bf
Shawon Talukder <shawon.talukder@green-delta.com>
2025-10-13 17:27:00 +0600

    updated with modal for privacy and terms

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { TermsAndConditionModal } from '../terms-and-condition-modal/terms-and-condition-modal';
+import { PrivacyPolicyModal } from '../privacy-policy-modal/privacy-policy-modal';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatButtonModule, MatDialogModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
+  readonly dialog = inject(MatDialog);
+
+  openDialogTermsAndCondition() {
+    const dialogRef = this.dialog.open(TermsAndConditionModal, {
+      width: '90vw',
+      maxWidth: '1200px',
+      maxHeight: '90vh',
+      disableClose: true,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogPrivacyPolicy() {
+    const dialogRef = this.dialog.open(PrivacyPolicyModal, {
+      width: '90vw',
+      maxWidth: '1200px',
+      maxHeight: '90vh',
+      disableClose: true,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
